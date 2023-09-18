93d819058acbb025de880937044b624b9dd3d260
Tolik <xiaohei@list.ru>
2023-09-18 21:27:30 +0800

    Задача 36

---
+++ DZ5/Zad34/Zad36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+int Sum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 1)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+int [] arr = getRandomArray(4,0,20);
+System.Console.WriteLine(string.Join(" ", arr));
+Console.WriteLine(Sum(arr));
